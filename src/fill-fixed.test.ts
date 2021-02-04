b35afe4ae3ebb7d960ef7f9396632d8706771338
Janko <simonovic86@gmail.com>
2021-02-04 17:55:07 +0100

    allow stubbing ethers.js contracts

--- src/fill-fixed.test.ts
+++ src/fill-fixed.test.ts
@@ -3,39 +3,74 @@ import { stub } from 'sinon'
 import { assert } from 'chai'
 import Vendor from './vendors/ethers'
 import Swivel from './swivel'
-import { getDefaultProvider } from "@ethersproject/providers";
-import { Wallet } from "@ethersproject/wallet";
+import { getDefaultProvider } from '@ethersproject/providers'
+import { Wallet } from '@ethersproject/wallet'
 import { Order, Components, TxResponse, Contract } from './interfaces'
+
+/**
+ * Defines a property with writable permission
+ */
+function defineWriteOnly<T, K extends keyof T>(object: T, name: K, value: T[K]): void {
+  Object.defineProperty(object, name, {
+    enumerable: false,
+    value: value,
+    writable: true,
+  });
+}
+
+/**
+ * Clone an object with all writable properties
+ */
+function cloneWithWriteAccess(o: any): any {
+  if (Array.isArray(o)) {
+    return o.map((item) => cloneWithWriteAccess(item))
+  }
+  if (typeof(o) === "object") {
+    const result: { [ key: string ]: any } = {}
+    for (const key in o) {
+      if (o.hasOwnProperty(key)) {
+        const value = o[key]
+        if (value === undefined) {
+          continue
+        }
+        defineWriteOnly(result, key, cloneWithWriteAccess(value))
+      }
+    }
+    return result
+  }
+  return o
+}
 
 describe('Swivel fillFixed method', () => {
   let swivel: Swivel
 
   before(() => {
-    const ethersProvider = getDefaultProvider();
-    const signer = Wallet.createRandom().connect(ethersProvider);
+    const ethersProvider = getDefaultProvider()
+    const signer = Wallet.createRandom().connect(ethersProvider)
     const vendor: Vendor = new Vendor(ethersProvider, signer)
     swivel = new Swivel(vendor)
     swivel.at('0xabc')
   })
-
 
   it('has a delegated fillFixed method', () => {
     assert.equal(typeof swivel.contract?.functions.fillFixed, 'function')
   })
 
   it('passes the call thru to the meta contract', async () => {
-    const response:TxResponse = { blockNumber: 789 }
+    // allow stubbing contract properties
+    swivel.contract = cloneWithWriteAccess(swivel.contract)
 
-    // init dummy contract
-    swivel.contract = {
-      address: '0xaddress',
-      functions: {
-        fillFixed: () => {},
-      }
+    const invalidContract = {
+      address: '0xinvalid',
+      functions: {}
     }
 
-    const fake = stub(swivel.contract.functions, 'fillFixed')
-    fake.resolves(response)
+    const contract: Contract = swivel.contract? swivel.contract : invalidContract
+    assert.isNotNull(contract)
+    assert.notDeepEqual(contract, invalidContract)
+
+    const fake = stub(contract.functions, 'fillFixed')
+    fake.resolves({ blockNumber: 789 })
 
     const order:Order = {
       key: '0xorder',
