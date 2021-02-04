7dff7adb204745098761190024ed89aebb08734a
Janko <simonovic86@gmail.com>
2021-02-04 19:27:38 +0100

    extract possible common helper functions to helpers.ts

--- src/fill-fixed.test.ts
+++ src/fill-fixed.test.ts
@@ -6,40 +6,7 @@ import Swivel from './swivel'
 import { getDefaultProvider } from '@ethersproject/providers'
 import { Wallet } from '@ethersproject/wallet'
 import { Order, Components, TxResponse, Contract } from './interfaces'
-
-/**
- * Defines a property with writable permission
- */
-function defineWriteOnly<T, K extends keyof T>(object: T, name: K, value: T[K]): void {
-  Object.defineProperty(object, name, {
-    enumerable: false,
-    value: value,
-    writable: true,
-  });
-}
-
-/**
- * Clone an object with all writable properties
- */
-function cloneWithWriteAccess(o: any): any {
-  if (Array.isArray(o)) {
-    return o.map((item) => cloneWithWriteAccess(item))
-  }
-  if (typeof(o) === "object") {
-    const result: { [ key: string ]: any } = {}
-    for (const key in o) {
-      if (o.hasOwnProperty(key)) {
-        const value = o[key]
-        if (value === undefined) {
-          continue
-        }
-        defineWriteOnly(result, key, cloneWithWriteAccess(value))
-      }
-    }
-    return result
-  }
-  return o
-}
+import { cloneWithWriteAccess } from "./helpers";
 
 describe('Swivel fillFixed method', () => {
   let swivel: Swivel
